3e72301133d710a6155332e45b561eb56f2d4376
maddiej608 <162855318+maddiej608@users.noreply.github.com>
2024-03-19 15:55:02 -0700

    Delete java script/script.js

--- java script/script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-
-const prevButton = document.getElementById("prevButton");
-const nextButton = document.getElementById("nextButton");
-nextButton.addEventListener('click', function () 
-							prevButton.addEventListener('click', function () 
