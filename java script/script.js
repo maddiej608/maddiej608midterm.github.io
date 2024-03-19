006ca6640acfa8bb9def41c88b1220208b4169c4
maddiej608 <162855318+maddiej608@users.noreply.github.com>
2024-03-19 15:25:06 -0700

    Add files via upload

---
+++ java script/script.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+nextButton.addEventListener('click', function () 
+							prevButton.addEventListener('click', function () 
